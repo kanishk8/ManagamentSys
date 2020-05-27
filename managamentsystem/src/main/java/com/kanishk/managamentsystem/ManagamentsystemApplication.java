326be70c18cb62f473cb30efbd0fa66178dd32ad
nEW u <nEW u@BT1605449>
2020-05-27 16:34:21 +0530

    Initial commit.  Configure the application properties

---
+++ managamentsystem/src/main/java/com/kanishk/managamentsystem/ManagamentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.kanishk.managamentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagamentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagamentsystemApplication.class, args);
+	}
+  
+}
